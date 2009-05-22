24e5a38c94b9878a81b1c6d6807443c461c3f055
hboutemy <hboutemy@71732bd6-1153-0410-a4df-d3775de70ba9>
2009-05-22 21:41:33 +0000

    [JAVANCSS-21] updated S_RCS_HEADER content to match Header format

--- src/main/java/javancss/Main.java
+++ src/main/java/javancss/Main.java
@@ -34,7 +34,8 @@ import java.util.Locale;
  */
 public class Main {
     private static final String IMPL_VERSION = Main.class.getPackage().getImplementationVersion();
-    public static final String S_RCS_HEADER = (IMPL_VERSION == null) ? "unknown version" : IMPL_VERSION;
+    public static final String S_RCS_HEADER = "$Header: /javancss/Main.java,v "
+        + ((IMPL_VERSION == null) ? "0.0 2001/01/01 00:00:00" : IMPL_VERSION) + " clemens Exp clemens $";
 
     public static void main(String[] asArgs) throws IOException {
         Locale.setDefault( Locale.US );
