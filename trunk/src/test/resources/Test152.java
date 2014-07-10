0ed495f4fac96feb7dccfa452c6a624f84a67d45
pkofler <pkofler@71732bd6-1153-0410-a4df-d3775de70ba9>
2014-07-10 00:32:50 +0000

    [JAVANCSS-37] add test from duplicate issue

--- trunk/src/test/resources/Test152.java
+++ trunk/src/test/resources/Test152.java
@@ -1,5 +1,8 @@
 package issues;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsInMethodInvocations 
 {
     public static void main( String[] args )
@@ -16,4 +19,16 @@ public class GenericsInMethodInvocations
     }
     
     private <Boolean> void bar() {}
+
+    void postCopyOnDestination(String str) throws InstantiationException, IllegalAccessException {
+       List<AllowedMMProduct> listToReset = new ArrayList<AllowedMMProduct>();
+       
+       List<AllowedMMProductAudit> auditProducts;
+       auditProducts = this.<AllowedMMProduct,AllowedMMProductAudit>copyListFromParent(AllowedMMProductAudit.class, getMmAuthorisedProducts_());
+    }
+    
+    List<AllowedMMProduct> getMmAuthorisedProducts_() {
+       return null;
+    }
+
 }
