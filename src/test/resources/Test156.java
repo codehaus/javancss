db172700857bbfd2cc2671ff27cc1c467eac9621
pkofler <pkofler@71732bd6-1153-0410-a4df-d3775de70ba9>
2014-07-10 23:12:20 +0000

    support hexadecimal floating-point literals with negative exponent

--- src/test/resources/Test156.java
+++ src/test/resources/Test156.java
@@ -5,6 +5,9 @@ public class HexadecimalFloatingPointLiterals
     {
         // the follow values come from the Double Javadoc/Spec
         assertEquals( "0x1.01p10", Double.toHexString( 0x1.01p10 ) );
+
+        assertTrue("Wrong value of java.lang.Double.MIN_NORMAL",
+                   Double.MIN_NORMAL == 0x1.0p-1022);
     }
 
 }
