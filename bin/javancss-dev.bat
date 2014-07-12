0b4e4616287d3e14085f2ca705756f05bf9deea2
pkofler <pkofler@71732bd6-1153-0410-a4df-d3775de70ba9>
2014-07-12 13:53:09 +0000

    added local run script for testing with the snapshot

---
+++ bin/javancss-dev.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+REM Please adapt this script to your environment.
+
+@REM Set JAVANCSS_HOME to the directory this batch file is in.
+set me=%0
+for %%i in (%me%) do set JAVANCSS_HOME=%%~dpi
+set JAVANCSS_HOME=%JAVANCSS_HOME%\..
+
+REM #################### EDIT THIS ENVIRONMENT VARIABLE IF NOT ALREADY SET #################
+set CLASSPATH=%JAVANCSS_HOME%\target\javancss-33.54-SNAPSHOT.jar;%JAVANCSS_HOME%\lib\ccl.jar;%JAVANCSS_HOME%\lib\jhbasic.jar;%CLASSPATH%
+
+"%JAVA_HOME%\bin\java" -classpath "%CLASSPATH%" javancss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+endlocal
