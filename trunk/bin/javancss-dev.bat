3530662794e1fdf1bed3b2da21178a833e08b277
pkofler <pkofler@71732bd6-1153-0410-a4df-d3775de70ba9>
2014-08-04 20:17:35 +0000

    fix extra \ in windows scripts, convert to Windows LF, update version in dev

--- trunk/bin/javancss-dev.bat
+++ trunk/bin/javancss-dev.bat
@@ -5,11 +5,11 @@ REM Please adapt this script to your environment.
 @REM Set JAVANCSS_HOME to the directory this batch file is in.
 set me=%0
 for %%i in (%me%) do set JAVANCSS_HOME=%%~dpi
-set JAVANCSS_HOME=%JAVANCSS_HOME%\..
+set JAVANCSS_HOME=%JAVANCSS_HOME%..
 
 REM #################### EDIT THIS ENVIRONMENT VARIABLE IF NOT ALREADY SET #################
-set CLASSPATH=%JAVANCSS_HOME%\target\javancss-33.54-SNAPSHOT.jar;%JAVANCSS_HOME%\lib\ccl.jar;%JAVANCSS_HOME%\lib\jhbasic.jar;%CLASSPATH%
+set CLASSPATH=%JAVANCSS_HOME%\target\javancss-34.55-SNAPSHOT.jar;%JAVANCSS_HOME%\lib\ccl.jar;%JAVANCSS_HOME%\lib\jhbasic.jar
 
-"%JAVA_HOME%\bin\java" -classpath "%CLASSPATH%" javancss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JAVA_HOME%\bin\java" -classpath %CLASSPATH% javancss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 endlocal
