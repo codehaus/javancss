b0d8a4a7c323fafcf0c7e418e0f4c0fee79a0cba
pkofler <pkofler@71732bd6-1153-0410-a4df-d3775de70ba9>
2014-08-04 20:17:35 +0000

    fix extra \ in windows scripts, convert to Windows LF, update version in dev

--- bin/javancss.bat
+++ bin/javancss.bat
@@ -4,7 +4,7 @@ REM Please adapt this script to your environment.
 @REM Set JAVANCSS_HOME to the directory this batch file is in.
 set me=%0
 for %%i in (%me%) do set JAVANCSS_HOME=%%~dpi
-set JAVANCSS_HOME=%JAVANCSS_HOME%\..
+set JAVANCSS_HOME=%JAVANCSS_HOME%..
 
 set _JAVA_HOME_ORIG=%JAVA_HOME%
 set _CLASSPATH_ORIG=%CLASSPATH%
@@ -15,9 +15,9 @@ if NOT "%JAVA_HOME%"=="" goto endif1
 :endif1
 
 REM #################### EDIT THIS ENVIRONMENT VARIABLE IF NOT ALREADY SET #################
-set CLASSPATH=%JAVANCSS_HOME%\lib\javancss.jar;%JAVANCSS_HOME%\lib\ccl.jar;%JAVANCSS_HOME%\lib\jhbasic.jar;%CLASSPATH%
+set CLASSPATH=%JAVANCSS_HOME%\lib\javancss.jar;%JAVANCSS_HOME%\lib\ccl.jar;%JAVANCSS_HOME%\lib\jhbasic.jar
 
-%JAVA_HOME%\bin\java -classpath %CLASSPATH% javancss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JAVA_HOME%\bin\java" -classpath %CLASSPATH% javancss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set JAVA_HOME=%_JAVA_HOME_ORIG%
 set CLASSPATH=%_CLASSPATH_ORIG%
